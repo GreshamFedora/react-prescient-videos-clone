5ab034fb63a535589207c55e6a319d0fec0fd872
Gresham <gresham.fedora@gmail.com>
2023-02-18 13:26:16 -0500

    working on ZH sidebar

--- src/components/ZhSidebar.jsx
+++ src/components/ZhSidebar.jsx
@@ -16,4 +16,4 @@ const ZhSidebar = ({ selectedCategory, setSelectedCategory }) => (
                 onClick={() => setSelectedCategory(category.name)}
                 className="category-btn"
                 style={{
-                background: category.name 
+                background: category.name === selectedCategory && '#FC1503'
